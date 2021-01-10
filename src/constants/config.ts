7699118daab730671f7a4654e2b74ab621d41554
Mikhaik Shautsou <mikhail.shautsou@gmail.com>
2021-01-11 02:57:20 +0300

    change port to env

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,2 +1,4 @@
-export const JWTSecret: string = process.env.jwtsecret || 'secret';
-export const APIUrl: string = process.env.apiurl || 'http://lockalhost:32342';
+export const JWTSecret: string = process.env.JWT_SECRET || 'secret';
+export const APIUrl: string = process.env.API_URL || 'http://localhost:8888';
+export const PORT: string = process.env.PORT || '8888';
+
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import * as bodyParser from 'body-parser';
 import path from 'path';
 import { PUBLIC, API } from './constants/routes';
+import { PORT } from './constants/config';
 import routers from './routes';
 
 const app = express();
-const PORT = 32342;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
